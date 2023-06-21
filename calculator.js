82a7de34a969544a5242716f471c20b0c15fc0a6
Christian Sarmiento <christian.sarmiento.work@gmail.com>
2023-06-22 04:51:11 +0800

    Minor change with the logic and function names. Fully functional calculator

--- calculator.js
+++ calculator.js
@@ -1,92 +1,153 @@
-var total = null;
-var previous = 0;
-var current = "";
-var operator = null;
 var display = document.getElementById("result")
-var cButton = document.getElementById("clear")
+var clearButton = document.getElementById("clear")
 var hasDecimal = false;
 var isNegated = false;
+var total = null;
+var current = 0;
+var previous = null;
+var operator = null;
+var toDisplayNewValue = false;
+var equalFlag = false;
 
 function clearDisplay() {
-    if (cButton.textContent == "AC") {
-        // reset all variables to default state/value
+    if (clearButton.textContent == "AC") {
         total = null;
-        previous = 0;
-        current = "";
-        operator = null;
-        lastTotal = 0;
     }
-    else cButton.textContent = "AC";
+    else {
+        clearButton.textContent = "AC";
+    }
 
-    current = ""
+    current = 0;
+    isNegated = false;
+    previous = null;
     display.textContent = "0";
-    hasDecimal = false; 
+    hasDecimal = false;
+    console.log(current);
 }
 
-// function negate() {
-//     current = display.textContent
-//     current = isNegated == false ? current = "-" + current : current = current.substring(1);
-//     isNegated = !isNegated
-//     display.textContent = current;
+function addDecimal() {
+    if (!hasDecimal) display.textContent += "."
+    current = parseFloat(display.textContent)
+    hasDecimal = true;
+}
 
-// }
+function addNegative() {
+    current *= -1;
+    isNegated = !isNegated
 
-function setDecimal() {
-    if (!hasDecimal) {
-        current === "" ? current += "0." : current += "."
-        display.textContent = current;
+    if (!toDisplayNewValue)
+        display.textContent = isNegated ? "-" + display.textContent : display.textContent.substring(1); 
+    else
+        // we should make the display content -0
+        display.textContent = "-0"
+}
+
+function calculate(equalWasPressed) {
+
+    switch(operator) {
+        case "+":
+            total = previous + current;    
+            break
+
+        case "-":
+            total = previous - current;
+            break
+
+        case "*":
+            total = previous * current;
+            break
+
+        case "/":
+            total = previous / current;
+            break
     }
-    hasDecimal = true; // to avoid multiple decimal placement
-    cButton.textContent = "C";
-}
 
-function setValue(value) {
-    if (current.length < 9) current += value;
+    // check if total is finite
+    if (!isFinite(total)) {
+        display.textContent = "Error";
+        return false
+    }
 
-    display.textContent = current;
-    cButton.textContent = "C";
+    let totalStr = total.toString()
+    
+    if (totalStr.length > 9)
+        if (operator == "*" || operator == "+")
+            totalStr = total.toExponential(0);
+        else
+            totalStr = totalStr.substring(0, 9)
+
+    display.textContent = totalStr;
+    // set previous value to total
+    // to calculate total and current when clear is not pressed yet 
+    previous = total;
+    toDisplayNewValue = false;
+    
+    if (equalWasPressed) equalFlag = equalWasPressed;
 }
 
 function setOperator(optr) {
     operator = optr;
+    toDisplayNewValue = true;
+
+    if (previous == null) {
+        previous = current;
+        current = 0;
+    }
+
+    if (previous > 0 && current > 0) {
+        // we want to clone apple's calculator behavior 
+        // where it do calculation when opeator was selected
+        // when both previous and current has values
+        // but we should not proceed here if equal button was pressed
+        if (!equalFlag)
+            calculate(false)
+            current = 0;
+    }
     
-    if (previous === 0) {
-        previous = total == null ? parseFloat(current) : parseFloat(total);
-    }
-    else if (previous > 0 && current.length > 0) {
-        calculate()
-    }
-    current = ""
+    // set equal was pressed back to default state
+    equalFlag = false;
+    hasDecimal = false; 
 }
 
-function calculate() {
-    current = parseFloat(current);
-    switch(operator) {
-        case "+":
-            if (!isNaN(current)) total = previous + current;
-            else total += total
-            break
 
-        case "-":
-            if (!isNaN(current)) total = previous - current;
-            else total -= total
-            break
-
-        case "*":
-            if (!isNaN(current)) total = (previous * current).toFixed(2);
-            else total *= total
-            break
-
-        case "/":
-            if (!isNaN(current)) total = (previous / current).toFixed(2);
-            else if (!isFinite(total)) total = "Error"
-            else total /= total
-            break
+function updateDisplay(value) {
+    
+    if (current === 0) {
+        // no decimal
+        if (!hasDecimal) {
+            if (!isNegated)
+                display.textContent = value;
+            else
+                display.textContent = "-" + value;
+        }
+        else {
+            if (display.textContent.length <= 8)
+                display.textContent += value;
+        }
     }
-    previous = total;
+    else {
+        if (display.textContent.length <= 8)
+            if (!toDisplayNewValue)
+                display.textContent += value
+            else 
+                display.textContent + value
+                toDisplayNewValue = false;
+    }
 
-    if (total.toString().length > 9) total = parseFloat(total).toExponential(0);
-    display.textContent = total.toString();
+    clearButton.textContent = "C";
+    current = parseFloat(display.textContent)
 }
+
+function setToPercentage() {
+    toConvert = (parseFloat(display.textContent) / 100).toString()
+    toStr = toConvert.toString()
+
+    if (toStr.length > 9)
+        toStr = toStr.substring(0, 9)
+
+    display.textContent = toStr;
+}
+
+
 
 
